7797be78870e1ae11e8b026092255af5965864f5
mukk38 <mustafakarakaya05@hotmail.com>
2020-09-28 01:04:49 +0300

    Leaflet
    İlk sürüm

---
+++ src/MyMap.js
@@ -0,0 +1,32 @@
+const { Map: LeafletMap, TileLayer, Marker, Popup } = ReactLeaflet
+
+class MyMap extends React.Component {
+  render() {
+    return (
+      <LeafletMap
+        center={[50, 10]}
+        zoom={6}
+        maxZoom={10}
+        attributionControl={true}
+                zoomControl={true}
+                doubleClickZoom={true}
+                scrollWheelZoom={true}
+                dragging={true}
+                animate={true}
+                easeLinearity={0.35}
+        >
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={[50, 10]}>
+          <Popup>
+            Popup for any custom information.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
+}
+
+
+ReactDOM.render(<MyMap />, document.getElementById('container'))
---
+++ src/RouteButonPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class RouteButonPage extends React.Component{
+    
+
+    render(){
+        return(
+            <h1>mustafa</h1>
+        );
+    }
+
+}
+
+export default RouteButonPage;
